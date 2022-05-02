a45099b0bcfaec7b281f649b83ffec22ef5a177f
Katarina Milanov <kaca.milanov@gmail.com>
2022-05-02 08:16:05 +0200

    dodate klase za boje u css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import logo from './logo.svg';
+import { useState } from 'react';
+import { Route, BrowserRouter, Routes } from 'react-router-dom'; 
 import './App.css';
 import Navbar from './components/Navbar';
 import Party from './pages/Party';
+import Istorija from './pages/Istorija';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [pokusaji, setPokusaji] = useState([]);
+  const dodajPokusaj = (pokusaj) => {
+    setPokusaji(prev => {
+      return [...prev, pokusaj]; 
+    })
+  }
+
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path='/statistika' element={<Statistika pokusaji={pokusaji} />} />
-
+        <Route path='/istorija' element={<Istorija pokusaji={pokusaji} />} />
+        <Route path='/' element={<Party pokusaji={pokusaji.slice(-10).reverse()} dodajPokusaj={dodajPokusaj} />} />
       </Routes>
     </BrowserRouter>
   );
